17ff2ff2dab3ac290711b144e2bb4b03413e2049
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-09-15 11:48:51 -0700

    refactor: move conversation repository logic from index.ts to repositories/ConversationRepository.ts

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,7 +3,7 @@ import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
 import z from 'zod';
-
+import { conversationRepository} from './repositories/conversation.repository';
 
 dotenv.config();
 
@@ -23,7 +23,7 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.send({ message: 'Hello World!' });
 });
 
-const conversation = new Map<string, string>();
+
 
 const chatSchema = z.object({
    prompt: z.string()
@@ -40,20 +40,26 @@ app.post('/api/chat', async (req: Request, res: Response) => {
       return;
    }
 
-   const {prompt, conversationId} =  req.body;
+   
+   try {
+      const {prompt, conversationId} =  req.body;
+      const response = await client.responses.create({
+         model: 'gpt-4o-mini!',
+         input: prompt,
+         temperature: 0.2,
+         max_output_tokens: 100,
+         previous_response_id: conversationRepository.getLastResponseId(conversationId)
+      });
+   
+      // lastresponseId = response.id;
+      conversationId.setLastResponseId(conversationId, response.id)
+   
+      res.json({message: response.output_text});
+      
+   } catch (error) {
+      res.status(500).json({error: 'Failed to generate a response.'});
+   }
 
-   const response = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input: prompt,
-      temperature: 0.2,
-      max_output_tokens: 100,
-      previous_response_id: conversation.get(conversationId)
-   });
-
-   // lastresponseId = response.id;
-   conversation.set(conversationId, response.id);
-
-   res.json({message: response.output_text});
 })
 
 app.listen(port, () => {
