f8a1407e0e88a143829d7b0b2dc1cc14c285d1d0
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-09-15 23:49:24 -0700

    Clean up index.ts

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,23 +1,11 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { chatController } from './controllers/chat.controller';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
-
-// app.get('/', (req: Request, res: Response) => {
-//    res.send('Hello World!');
-// });
-
-// app.get('/api/hello', (req: Request, res: Response) => {
-//    res.send({ message: 'Hello World!' });
-// });
-
-// app.post('/api/chat', chatController.sendMessage)
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
