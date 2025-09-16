3e166c01f75e856fa5d85495f6e3e78a43378925
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-09-15 23:32:49 -0700

    Remove comments from index.ts

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -18,32 +18,7 @@ app.get('/api/hello', (req: Request, res: Response) => {
    res.send({ message: 'Hello World!' });
 });
 
-
-
-// const chatSchema = z.object({
-//    prompt: z.string()
-//    .trim()
-//    .min(1, 'Prompt is required.')
-//    .max(1000, 'Prompt is too long(max is 1000 characters)'),
-//    conversationId: z.uuid({ message: 'Invalid conversation ID' })
-// });
-
 app.post('/api/chat', async (req: Request, res: Response) => {
-   // const parseResult = chatSchema.safeParse(req.body);
-   // if(!parseResult.success){
-   //    res.status(400).json(parseResult.error.format());
-   //    return;
-   // }
-   
-   // try {
-   //    const {prompt, conversationId} =  req.body;
-
-   //    const response = await chatService.sendMessage(prompt, conversationId);
-   //    res.json({message: response.message});
-      
-   // } catch (error) {
-   //    res.status(500).json({error: 'Failed to generate a response.'});
-   // }
 
 })
 
