817be7b5bc6a851a71fa20cbe703bedd0d1b7eeb
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-09-15 23:47:20 -0700

    Create route.ts

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -9,15 +9,15 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!');
-});
+// app.get('/', (req: Request, res: Response) => {
+//    res.send('Hello World!');
+// });
 
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.send({ message: 'Hello World!' });
-});
+// app.get('/api/hello', (req: Request, res: Response) => {
+//    res.send({ message: 'Hello World!' });
+// });
 
-app.post('/api/chat', chatController.sendMessage)
+// app.post('/api/chat', chatController.sendMessage)
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
