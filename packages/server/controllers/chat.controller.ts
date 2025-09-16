9e80ae2fcb17fdfde04e1f2633bcf10bf569dc28
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-09-15 23:29:14 -0700

    feat: implement chatController with validation and service integration

--- packages/server/controllers/chat.controller.ts
+++ packages/server/controllers/chat.controller.ts
@@ -1,5 +1,34 @@
-import type { Request } from "express";
+import type { Request, Response } from "express";
+import { chatService } from "../services/chat.service";
+import z from "zod";
 
+//Implementation detail
+const chatSchema = z.object({
+    prompt: z.string()
+    .trim()
+    .min(1, 'Prompt is required.')
+    .max(1000, 'Prompt is too long(max is 1000 characters)'),
+    conversationId: z.uuid({ message: 'Invalid conversation ID' })
+ });
+ 
+//public interface
 export const chatController = {
-    sendMessage(req: Request, res: Response) {}
+    async sendMessage(req: Request, res: Response) {
+        const parseResult = chatSchema.safeParse(req.body);
+        if(!parseResult.success){
+            res.status(400).json(parseResult.error.format());
+            return;
+        }
+        
+        try {
+            const {prompt, conversationId} =  req.body;
+
+            const response = await chatService.sendMessage(prompt, conversationId);
+            res.json({message: response.message});
+            
+        } catch (error) {
+            res.status(500).json({error: 'Failed to generate a response.'});
+        }
+
+    }
 }
